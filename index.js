7ec6b583a59be8b31edb72820787ca04154321e0
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-04-30 17:08:07 +0100

    configuracao EJS e Bootstrap

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 
 //Dizendo para o express usar o ejs como view engine
+//express he configurado para buscar o html na pasta views
 app.set('view engine', 'ejs');
 
+//setando para o express saber qual pasta ira conter os
+//arquivos staticos ex: css, imagens e etc
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.send("Bem vindo ao site!");
+    res.render("index");
 });
 
 app.listen("8080", () => {
