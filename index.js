03f917dbe1c3415a9db2fdcbc6ee98e7c9e798d5
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-05-01 14:12:53 +0100

    instalacao e uso do body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Dizendo para o express usar o ejs como view engine
 //express he configurado para buscar o html na pasta views
@@ -9,6 +10,11 @@ app.set('view engine', 'ejs');
 //arquivos staticos ex: css, imagens e etc
 app.use(express.static('public'));
 
+//setando o body-parser ao express
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -17,6 +23,14 @@ app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 });
 
+app.post("/salvarPergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var pergunta = req.body.pergunta;
+
+    res.send("Titulo: "+titulo+"</br>Pergunta: "+pergunta);
+});
+
+//Start server
 app.listen("8080", () => {
     console.log("Servidor rodando!");
 });
